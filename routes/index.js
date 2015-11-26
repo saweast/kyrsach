c31d0dc4a4f1eb154efeaeb9a68406cc75a8f5c8
Vadim Lavorchuk <lavorchukvadim@yandex.ua>
2015-11-26 02:08:13 +0200

    work with cookies

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var indx = express();
+var cookieParser = require('cookie-parser');
+
+indx.use(cookieParser());
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.cookie('name', 'Vadim', {});
+  var nm = req.cookies.name;
+  res.render('index', {title: 'Express', name: nm});
 });
 
 module.exports = router;
